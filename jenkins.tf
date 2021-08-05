3a8773e6f09d5eb842b2fd970544dbf03c727fb6
AyushGupta88 <ayush.gupta@oodles.io>
2021-08-05 13:08:54 +0530

    provisioner issue solved

--- jenkins.tf
+++ jenkins.tf
@@ -9,10 +9,21 @@ resource "aws_key_pair" "kp" {
   public_key = tls_private_key.pk.public_key_openssh
 
   provisioner "local-exec" { 
-  // Create a "myKey.pem" to your computer!!
+  // Create a "Key.pem" to your computer!!
     command = "echo '${tls_private_key.pk.private_key_pem}' > ./key.pem"
+    //command = "chmod 400 ./key.pem"
+  }
+    provisioner "local-exec" {
+    command = "chmod 400 ./'${var.key_name}'.pem"
   }
 }
+//resource "null_resource" "change_permission" {
+
+// provisioner "local-exec" {
+
+//    command = "sudo chmod 400 /home/ayush/Desktop/terraform/ECSDeploy/new/key.pem"
+//  }
+//}
 resource "aws_security_group" "allow_tls" {
   name        = var.secgrp_name
   description = "Allow TLS inbound traffic"
@@ -65,27 +76,38 @@ resource "aws_instance" "test" {
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
   key_name = aws_key_pair.kp.key_name
 
-  provisioner "local-exec" {
-      command = "chmod 400 /home/ayush/Downloads/var.key_name}"
-  }
+
   provisioner "remote-exec" {
         connection {
           # The default username for our AMI
           user = "ubuntu"
           host = "${self.public_ip}"
           type     = "ssh"
-          private_key = "${file("/home/ayush/Downloads/var.key_name.pem")}"
+          //private_key = tls_private_key.pk.public_key_openssh
+          private_key = "${file("/home/ayush/Desktop/terraform/ECSdeploy/new/key.pem")}"
         }
 
         inline = [
           "sudo apt-get -y update",
           "sudo apt-get -y install docker.io",
-          "sudo apt-get -y install docker-compose",
+          "sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose",
+          "sudo chmod +x /usr/local/bin/docker-compose",
+          "docker-compose --version",
           "sudo chmod 666 /var/run/docker.sock",
           "sudo mkdir -p /opt/docker",
-          "sudo cp docker-compose.yml /opt/docker",
-          "cd /opt/docker && docker-compose up -d"
+          "sudo chown -R ubuntu:ubuntu /opt/docker"
         ]
+      }
+  provisioner "file" {
+        source      = "/home/ayush/Desktop/ECSdeploy/new/docker-compose.yml"
+        destination = "/opt/docker/"
+
+        connection {
+          type     = "ssh"
+          user     = "ubuntu"
+          private_key = "${file("/home/ayush/Desktop/terraform/ECSdeploy/new/key.pem")}"
+          host     = "${self.public_ip}"
+        }
       }
   /*user_data = <<-EOF
                 #! /bin/bash
